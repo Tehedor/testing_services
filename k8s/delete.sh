b310850f95fc6410591a7f7fbcb19f26f3036893
Sergio <sergiot1805@gmail.com>
2024-11-26 11:24:12 +0100

    deploys/

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -2,32 +2,32 @@
 
 # Eliminar despliegue del frontend
 echo "Eliminando despliegue del frontend..."
-kubectl delete -f frontend-deployment.yaml
+kubectl delete -f deploys/frontend-deployment.yaml
 
 # Eliminar despliegue del backend
 echo "Eliminando despliegue del backend..."
-kubectl delete -f backend-deployment.yaml
+kubectl delete -f deploys/backend-deployment.yaml
 
 # Eliminar despliegue de MySQL
 echo "Eliminando despliegue de MySQL..."
-kubectl delete -f db-mysql/mysql-deployment.yaml
+kubectl delete -f deploys/db-mysql/mysql-deployment.yaml
 
 # Eliminar despliegue de MongoDB
 echo "Eliminando despliegue de MongoDB..."
-kubectl delete -f db-mongo/mongo-deployment.yaml
+kubectl delete -f deploys/db-mongo/mongo-deployment.yaml
 
 # Eliminar Persistent Volumes para MySQL (si es necesario)
 echo "Eliminando Persistent Volumes..."
-kubectl delete -f db-mysql/mysql-persistent-volume.yaml
+kubectl delete -f deploys/db-mysql/mysql-persistent-volume.yaml
 
 # Eliminar Persistent Volume Claims para MongoDB y MySQL
 echo "Eliminando Persistent Volume Claims..."
-kubectl delete -f db-mongo/mongo-persistent-volume-claims.yaml
-kubectl delete -f db-mysql/mysql-persistent-volume-claims.yaml
+kubectl delete -f deploys/db-mongo/mongo-persistent-volume-claims.yaml
+kubectl delete -f deploys/db-mysql/mysql-persistent-volume-claims.yaml
 
 # Eliminar ConfigMaps para MongoDB y MySQL
 echo "Eliminando ConfigMaps..."
-kubectl delete -f db-mongo/mongo-seeder.yaml
-kubectl delete -f db-mysql/mysql-seeder.yaml
+kubectl delete -f deploys/db-mongo/mongo-seeder.yaml
+kubectl delete -f deploys/db-mysql/mysql-seeder.yaml
 
 echo "Despliegues eliminados correctamente."
--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,35 +3,35 @@
 # Aplicar ConfigMaps para MongoDB y MySQL
 echo "Aplicando ConfigMaps..."
 # kubectl apply -f db-mongo/mongo-seeder.yaml
-kubectl apply -f db-mysql/mysql-seeder.yaml
+kubectl apply -f deploys/db-mysql/mysql-seeder.yaml
 
 # Aplicar Persistent Volume Claims para MongoDB y MySQL
 echo "Aplicando Persistent Volume Claims..."
 # kubectl apply -f db-mongo/mongo-persistent-volume-claims.yaml
-kubectl apply -f db-mysql/mysql-persistent-volume-claims.yaml
+kubectl apply -f deploys/db-mysql/mysql-persistent-volume-claims.yaml
 
 # Aplicar Persistent Volumes para MySQL (si es necesario)
 echo "Aplicando Persistent Volumes..."
-kubectl apply -f db-mysql/mysql-persistent-volume.yaml
+kubectl apply -f deploys/db-mysql/mysql-persistent-volume.yaml
 
 # Aplicar despliegue de MongoDB
 echo "Aplicando despliegue de MongoDB..."
-# kubectl apply -f db-mongo/mongo-deployment.yaml
+# kubectl apply -f deploys/db-mongo/mongo-deployment.yaml
 
 # Crear secretos para MySQL
 echo "Creando secretos para MySQL..."
-bash db-mysql/create_secrets.sh
+bash db-deploys/mysql/create_secrets.sh
 
 # Aplicar despliegue de MySQL
 echo "Aplicando despliegue de MySQL..."
-kubectl apply -f db-mysql/mysql-deployment.yaml
+kubectl apply -f deploys/db-mysql/mysql-deployment.yaml
 
 # Aplicar despliegue del backend
 echo "Aplicando despliegue del backend..."
-kubectl apply -f backend-deployment.yaml
+kubectl apply -f deploys/backend-deployment.yaml
 
 # Aplicar despliegue del frontend
 echo "Aplicando despliegue del frontend..."
-kubectl apply -f frontend-deployment.yaml
+kubectl apply -f deploys/   frontend-deployment.yaml
 
 echo "Despliegues aplicados correctamente."
